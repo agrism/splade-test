8387edb504bcf7647e2be5034530b6e9a0635a90
Agris <7924@inbox.lv>
2023-01-08 01:14:53 +0200

    lazy loading

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,28 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use ProtoneMedia\Splade\Facades\SEO;
+use ProtoneMedia\Splade\Facades\Splade;
 use ProtoneMedia\Splade\Facades\Toast;
 
 class IndexController extends Controller
 {
     public function __invoke(Request $request): View
     {
-        if(!$request->noToast){
+        if (!$request->noToast) {
             $this->toast($request);
         }
 
-//        $this->seo();
+        $this->seo();
 
         sleep(1);
 
-        return view('index');
+        return view('index', [
+            'items' => Splade::onLazy(function () {
+                $items = [];
+                foreach (range(0,5) as $ignored){
+                    $items[] = ['name' => uniqid()];
+                }
+                return $items;
+            }),
+        ]);
     }
 
     private function toast(Request $request): void
     {
         $toast = Toast::title('You are at Home!');
 
-        switch ($request->type){
+        switch ($request->type) {
             case 'danger':
                 $toast->danger();
                 break;
@@ -35,15 +44,15 @@ class IndexController extends Controller
                 break;
         }
 
-        if($request->backdrop){
+        if ($request->backdrop) {
             $toast->backdrop();
         }
 
-        if($request->leftTop){
+        if ($request->leftTop) {
             $toast->leftTop();
         }
 
-        if($request->center){
+        if ($request->center) {
             $toast->center();
         }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,5 +26,38 @@
     </ul>
 
 
+    <x-splade-lazy style="text-align: center;;">
+        <x-slot:placeholder> The items are loading... </x-slot:placeholder>
+
+        <ul>
+        @foreach($items as $item)
+            <li>{{data_get($item, 'name')}}</li>
+        @endforeach
+        </ul>
+    </x-splade-lazy>
+
+    <div>
+        <x-splade-toggle>
+            <button class="bg-transparent hover:bg-blue-900 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" @click.prevent="toggle">
+                Toggle items
+            </button>
+
+            <x-splade-lazy show="toggled" style="text-align: center;">
+                <x-slot:placeholder>
+                    <p>Loading items...</p>
+                </x-slot:placeholder>
+
+                <ul>
+                    @foreach($items as $item)
+                        <li>{{data_get($item, 'name')}}</li>
+                    @endforeach
+                </ul>
+            </x-splade-lazy>
+        </x-splade-toggle>
+    </div>
+
+
+
+
 </x-guest-layout>
 
