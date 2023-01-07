620becac3f126fffbce777773fc4f434bbfa221b
Agris <7924@inbox.lv>
2023-01-08 00:19:20 +0200

    add seo example

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,18 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use ProtoneMedia\Splade\Facades\SEO;
 use ProtoneMedia\Splade\Facades\Toast;
 
 class IndexController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): View
     {
-        if($request->noToast){
-            return view('index');
+        if(!$request->noToast){
+            $this->toast($request);
         }
+
+        $this->seo();
 
         sleep(1);
 
+        return view('index');
+    }
+
+    private function toast(Request $request): void
+    {
         $toast = Toast::title('You are at Home!');
 
         switch ($request->type){
@@ -39,7 +48,12 @@ class IndexController extends Controller
         }
 
         $toast->autoDismiss(2);
+    }
 
-        return view('index');
+    private function seo(): void
+    {
+        SEO::title('Laravel Splade Test')
+            ->description('testing usage of Splade')
+            ->keywords('laravel, splade, keywords');
     }
 }
